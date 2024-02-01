c99d1bfb10be76520f190ef16bc94c634c1ab5dc
Federico Moreno <fede_moreno613@hotmail.com>
2024-02-01 14:30:49 -0300

    (fix) datframe of metrics

--- takehome/model_utils.py
+++ takehome/model_utils.py
@@ -28,7 +28,7 @@ def evaluate_model(model, df_val, df_test, model_cols, labels_val, labels_test, 
     metrics_dict_test = compute_binary_score(labels_test, pred_test)
     metrics_dict_test["name"] = name
     metrics_dict_test["stage"] = "test"
-    metrics_dict_val["model"] = model_type
+    metrics_dict_test["model"] = model_type
     estimator_metrics.append(metrics_dict_test)
 
     return estimator_metrics

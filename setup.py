57e5dd65c62eeaf71f2d2aa592d147ec3f8c5b96
Federico Moreno <fede_moreno613@hotmail.com>
2024-02-01 10:08:54 -0300

    (fix) cuml package

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(
     name="takehome",
-    version="0.1",
+    version="1",
     packages=find_packages(),
     author="Federico Moreno",
     author_email="federicomoreno613@gmail.com",
     description="Takehome project ",
-    url="https://github.com/federicomoreno613/",
+    url="https://github.com/federicomoreno613/mltake",
     scripts=['scripts/takehome-run'],
     install_requires=requirements
 )
